64ed8a44e4f276d24066226b39f9309ae5d8fcbc
ljh9 <dkssud2208@naver.com>
2022-10-17 00:31:19 +0900

    mongodb connection

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://ljh:q123123@boilerplate.ovhwhqz.mongodb.net/?retryWrites=true&w=majority', {
+    // userNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
