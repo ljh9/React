ee6ccb0d5e7fe73b3bb2fb26c6e1683b97b23bd2
ljh9 <dkssud2208@naver.com>
2022-10-18 01:16:43 +0900

    BodyParser, PostMan, signup

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://ljh:q123123@boilerplate.ovhwhqz.mongodb.net/?retryWrites=true&w=majority', {
@@ -12,6 +17,19 @@ mongoose.connect('mongodb+srv://ljh:q123123@boilerplate.ovhwhqz.mongodb.net/?ret
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+  
 })
 
 app.listen(port, () => {
