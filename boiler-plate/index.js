072a71892c830172728b8c9cd3a3cf713679dbc6
ljh9 <dkssud2208@naver.com>
2022-10-20 00:38:12 +0900

    Generate token with jsonwebtoken

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
@@ -9,6 +10,7 @@ const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -49,11 +51,13 @@ app.post('/login', (req, res) => {
         return res.json({ loginSuccess: false, message: "비번틀림." })
 
       user.generateToken((err, user) => {
-        
+        if(err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token) 
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id })
       })
-
     })
-
   })
 })
 
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -54,6 +55,17 @@ userSchema.methods.comparePassword = function(plainPassword, cb) {
         if(err) return cb(err), 
             cb(null, isMatch)
     })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+
 }
 
 const User = mongoose.model('User', userSchema)
