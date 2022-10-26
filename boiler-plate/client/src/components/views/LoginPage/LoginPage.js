e852c4cdadf7ca22454abb9259a38725c556fbcf
ljh9 <dkssud2208@naver.com>
2022-10-26 21:51:14 +0900

    LoginPage

--- boiler-plate/client/src/components/views/LoginPage/LoginPage.js
+++ boiler-plate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
+import Axios from 'axios'
+import {useDispatch} from 'react-redux';
+import {loginUser} from '../../../_actions/user_action';
 
-function LoginPage() {
+function LoginPage(props) {
+  const dispatch = useDispatch();
 
   const [Email, setEmail] = useState("")
   const [Password, setPassword] = useState("")
@@ -14,6 +18,23 @@ function LoginPage() {
   }
 
   const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    let body = {
+      email: Email,
+      password: Password
+    }
+    
+    dispatch(loginUser(body))
+    .then(response => {
+      if(response.payload.loginSuccess){
+        props.history.push('/')
+      }else{
+        alert('Error')
+      }
+    })
+
+    
   }
 
   return (
@@ -24,9 +45,9 @@ function LoginPage() {
       <form style={{ display: 'flex', flexDirection: 'column'}}
         onSubmit={onSubmitHandler}
       >
-        <labe>Email</labe>
+        <label>Email</label>
         <input type="email" value={Email} onChange={onEmailHandler} />
-        <labe>Password</labe>
+        <label>Password</label>
         <input type="password" value={Password} onChange={onPasswordHandler} />
         <br />
         <button type="submit">
