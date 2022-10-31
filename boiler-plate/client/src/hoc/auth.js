efcce9c2a61e2b7158158eac1b654224711ba32e
ljh9 <dkssud2208@naver.com>
2022-10-31 21:51:40 +0900

    Auth check

---
+++ boiler-plate/client/src/hoc/auth.js
@@ -0,0 +1,19 @@
+import { Axios } from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { auth } from '../_actions/user_action';
+
+export default function (SpecificComponent, option, adminRoute = null){
+
+    function AuthenticationCheck(props){
+        const dispatch = useDispatch();
+
+        useEffect(() => {
+            dispatch(auth()).then(response => {
+                
+            })
+        }, [])
+    }
+
+    return AuthenticationCheck
+}
