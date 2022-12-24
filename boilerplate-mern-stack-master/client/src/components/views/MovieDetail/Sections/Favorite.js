40356710fdb3444efa167c97313fc51eda49ae66
ljh9 <dkssud2208@naver.com>
2022-12-24 13:50:23 +0900

    Add Favorite

--- boilerplate-mern-stack-master/client/src/components/views/MovieDetail/Sections/Favorite.js
+++ boilerplate-mern-stack-master/client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 
 function Favorite(props) {
@@ -9,6 +9,10 @@ function Favorite(props) {
     const moviePost = props.movieInfo.backdrop_path
     const movieRunTime = props.movieInfo.runtime
 
+
+    const [FavoriteNumber, setFavoriteNumber] = useState(0)
+    const [Favorited, setFavorited] = useState(false)
+
     useEffect(() => {
         let variable = {
             userFrom, 
@@ -17,16 +21,26 @@ function Favorite(props) {
         Axios.post('/api/favorite/favoriteNumber', variable)
         .then(response => {
             if(response.data.success) {
-                
+                setFavoriteNumber(response.data.favoriteNumber)
             } else {
                 alert('실패')
             }
         })
+
+        Axios.post('/api/favorite/favorited', variable)
+        .then(response => {
+            if(response.data.success) {
+                setFavorited(response.data.favorited)
+            } else {
+                alert('실패')
+            }
+        })
+
     }, [])
 
   return (
     <div>
-        <button>Favorite</button>
+        <button>{Favorited ? " Not Favorite" : "Add to Favorite"} {FavoriteNumber} </button>
     </div>
   )
 }
--- boilerplate-mern-stack-master/server/routes/favorite.js
+++ boilerplate-mern-stack-master/server/routes/favorite.js
@@ -14,4 +14,21 @@ router.post('/favoriteNumber', (req, res) => {
         })
 })
 
+router.post('/favorited', (req, res) => {
+    req.body.movieId
+
+
+    Favorite.find({ "movieId": req.body.movieId, "userFrom": req.body.userFrom })
+        .exec((err, info) => {
+            if(err) return res.status(400).send(err)
+
+            let result = false;
+            if(info.length !== 0) {
+                result = true
+            }
+
+            res.status(200).json({ success: true, favorited: result })
+        })
+})
+
 module.exports = router;
