45c10d77e3a1aad133cc362ce3c21825be04a0e6
ljh9 <dkssud2208@naver.com>
2022-12-27 16:52:59 +0900

    Add FavoritePage Data

--- boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
+++ boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import Axios from 'axios';
+import React, { useEffect } from 'react'
 import './favorite.css';
 
 function FavoritePage() {
+
+  useEffect(() => {
+    Axios.post('/api/favorite/getFavoredMovei', { useFrom: localStorage.getItem('userid') })
+    .then(response => {
+      if(response.data.success){
+
+      } else {
+        alert('실패')
+      }
+    })
+  }, [])
+
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
         <h2>Favorite Movies</h2>
--- boilerplate-mern-stack-master/server/routes/favorite.js
+++ boilerplate-mern-stack-master/server/routes/favorite.js
@@ -41,4 +41,12 @@ router.post('/addToFavorite', (req, res) => {
     })
 })
 
+router.post('/getFavoriedmovie', (req, res) => {
+    Favorite.find({ 'userFrom': req.body.userFrom })
+        .exec((err, favorites) => {
+            if(err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, favorites})
+        })
+})
+
 module.exports = router;
