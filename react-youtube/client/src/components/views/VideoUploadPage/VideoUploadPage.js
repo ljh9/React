16f99e21badddf25607c7376e2187bdccf6c107d
ljh9 <dkssud2208@naver.com>
2023-01-03 12:28:36 +0900

    Add UploadPage

--- react-youtube/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ react-youtube/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+
+const {TextArea} = Input;
+const {Title} = Typography;
 
 function VideoUploadPage() {
   return (
-    <div>VideoUploadPage</div>
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+      <Form onSubmit>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <img></img>
+          </div>
+        </div>
+        <br />
+        <br />
+        <label>Title</label>
+        <Input 
+          onChange
+          value
+        />
+        <br />
+        <br />
+        <label>Title</label>
+        <TextArea 
+          onChange
+          value
+        />
+        <br />
+        <br />
+        <select onChange>
+          <option key value></option>
+        </select>
+        <Button type='primary' size='large' onClick>
+          Submit
+        </Button>
+      </Form>
+    </div>
   )
 }
 
