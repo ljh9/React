80d37b5cc66bf996148a744504aa45eb6c79d002
ljh9 <dkssud2208@naver.com>
2023-01-05 21:58:44 +0900

    Update UploadPage

--- react-youtube/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ react-youtube/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
 
 const {TextArea} = Input;
 const {Title} = Typography;
 
+const PrivateOptions = [
+  {value: 0, label: "Private"}, 
+  {value: 1, label: "Public"}
+]
+
+const CategoryOptions = [
+  {value: 0, label: "Film & Animation"}, 
+  {value: 1, label: "Autos & Vehicles"}, 
+  {value: 2, label: "Music"}, 
+  {value: 3, label: "Pets & Animals"}
+]
+
 function VideoUploadPage() {
+
+  const [VideoTitle, setVideoTitle] = useState("")
+  const [Description, setDescription] = useState("")
+  const [Private, setPrivate] = useState(0)
+  const [Category, setCategory] = useState("Film & Animation")
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -36,19 +54,28 @@ function VideoUploadPage() {
         <label>Title</label>
         <Input 
           onChange
-          value
+          value={VideoTitle}
         />
         <br />
         <br />
-        <label>Title</label>
+        <label>Description</label>
         <TextArea 
           onChange
-          value
+          value={Description}
         />
         <br />
         <br />
         <select onChange>
-          <option key value></option>
+          {PrivateOptions.map((item, index) => (
+            <option key={index} value={item.value}>{item.label}</option>
+          ))}
+        </select>
+        <br />
+        <br />
+        <select onChange>
+          {CategoryOptions.map((item, index) => (
+            <option key={index} value={item.value}>{item.label}</option>
+          ))}
         </select>
         <Button type='primary' size='large' onClick>
           Submit
