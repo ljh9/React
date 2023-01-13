6f97cc654257116530f704c880fa1075d769efad
ljh9 <dkssud2208@naver.com>
2023-01-13 14:40:51 +0900

    Add LandingPage

--- react-youtube/client/src/components/views/LandingPage/LandingPage.js
+++ react-youtube/client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import { FaCode } from "react-icons/fa";
+import { Card, Avatar, Col, Typography, Row } from 'antd';
+const { Title } = Typography;
+const { Meta } = Card;
 
 function LandingPage() {
+
+    useEffect(() => {
+        axios.get('/api/video/getVideos')
+            .then(response => {
+                if (response.data.success) {
+                    console.log(response.data.videos)
+                } else {
+                    alert('Failed to get Videos')
+                }
+            })
+    }, [])
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <Title level={2} > Recommended </Title>
+            <hr />
+
+            <Row gutter={16}>
+                {renderCards}
+            </Row>
+        </div>
     )
 }
 
