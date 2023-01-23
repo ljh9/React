3d0d6736256c11fe3a174bdf0f5e04433a7bc895
ljh9 <dkssud2208@naver.com>
2023-01-23 16:02:30 +0900

    Add SubPage

---
+++ react-youtube/client/src/components/views/SubscriptionPage/SubscriptionPage.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import { FaCode } from "react-icons/fa";
+import { Card, Avatar, Col, Typography, Row } from 'antd';
+import axios from 'axios';
+import moment from 'moment';
+const { Title } = Typography;
+const { Meta } = Card;
+
+function SubscriptionPage() {
+  return (
+    <div>SubscriptionPage</div>
+  )
+}
+
+export default SubscriptionPage
