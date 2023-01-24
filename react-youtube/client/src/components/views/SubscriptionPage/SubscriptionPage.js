68f00b604eb47203bc513eec30efaa6024824f11
ljh9 <dkssud2208@naver.com>
2023-01-24 13:27:48 +0900

    Add SubPage

--- react-youtube/client/src/components/views/SubscriptionPage/SubscriptionPage.js
+++ react-youtube/client/src/components/views/SubscriptionPage/SubscriptionPage.js
@@ -6,10 +6,24 @@ import moment from 'moment';
 const { Title } = Typography;
 const { Meta } = Card;
 
-function SubscriptionPage() {
-  return (
-    <div>SubscriptionPage</div>
-  )
-}
+const [Videos, setVideos] = useState([])
+
+    let variable = { userFrom : localStorage.getItem('userId')  }
+
+    useEffect(() => {
+        axios.post('/api/video/getSubscriptionVideos', variable)
+            .then(response => {
+                if (response.data.success) {
+                    setVideos(response.data.videos)
+                } else {
+                    alert('실패')
+                }
+            })
+    }, [])
+
+  
+    return (
+        <div></div>
+    )
 
 export default SubscriptionPage
